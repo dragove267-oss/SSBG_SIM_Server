13daf90023dd0ee3588ad8195106004daf32eaa8
DOHA <decalcomanic@gmail.com>
2026-05-28 05:28:04 +0900

    원격 검증 스크립트 상세 출력 개선

--- game-server/verify_remote.js
+++ game-server/verify_remote.js
@@ -1,4 +1,9 @@
 const db = require("./database/db.js");
-const items = db.prepare("SELECT itemCode, name, itemType FROM item_definitions LIMIT 15;").all();
-console.log("Remote Items in DB:", items);
+const items = db.prepare("SELECT itemCode, name, itemType, description FROM item_definitions ORDER BY itemCode ASC;").all();
+console.log("\n=== 100% KOREAN ITEM DICTIONARY CATALOG ===");
+items.forEach(item => {
+  console.log(`[${item.itemCode}] [${item.itemType}] ${item.name} - ${item.description}`);
+});
+console.log("Total Items Seeded:", items.length);
 db.close();
+
